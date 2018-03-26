c576b471bdb8602200d88e9b2a291cec44bfa622
leooleo <leo.ms097@gmail.com>
2018-03-26 15:27:20 -0300

    Add compile script

---
+++ script.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+cd libopenbasn
+mkdir build
+cd build
+cmake ..
+make
+sudo make install
+cd ..
+cd ..
+
+cd bodyhub
+mkdir build
+cd build
+cmake ..
+make
+cd ..
+cd ..
+
+cd sensornode
+mkdir build
+cd build
+cmake ..
+make
+cd ..
+cd ..
