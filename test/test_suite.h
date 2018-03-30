eec3cf89f48de2fe52053d669e23eca5f5f0d6d2
gabrielevi10 <gabrielevigomes@gmail.com>
2018-03-30 16:43:19 -0300

    Arquivos objetos gerados pelo make estao na pasta tests/build Os .o estarão em tests/build até encontrarmos uma melhor maneira de utilizar o g++ em diretorios acima do atual.

--- test/test_suite.h
+++ test/test_suite.h
@@ -2,19 +2,19 @@
 #include <stdint.h>
 #include "../bodyhub/include/BodyHubModule.h"
             
-  using namespace std;
-  class MyTestSuite : public CxxTest::TestSuite{
+using namespace std;
+  
+class MyTestSuite : public CxxTest::TestSuite{
   public:
 
-      void testOpd( void ){
-        char arg0[] = "main";
-        char  arg1[] = "--cid=111";
-        char* argv[] = { &arg0[0], &arg1[0],NULL};
-        int32_t   argc   = (int32_t)(sizeof(argv) / sizeof(argv[0])) - 1;
+    void testOpd( void ){
+      char arg0[] = "main";
+      char  arg1[] = "--cid=111";
+      char* argv[] = { &arg0[0], &arg1[0],NULL};
+      int32_t   argc   = (int32_t)(sizeof(argv) / sizeof(argv[0])) - 1;
         
-        BodyHubModule inst(argc, argv);
-        inst.m_id = 1;
-        TS_ASSERT_EQUALS(inst.m_id, 1);  
-        
-      }
-  };
+      BodyHubModule inst(argc, argv);
+      inst.m_id = 1;
+      TS_ASSERT_EQUALS(inst.m_id, 1);    
+    }
+};
