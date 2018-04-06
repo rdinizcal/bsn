65b151492dd6e80fb855f572321a053321e0226c
gabrielevi10 <gabrielevigomes@gmail.com>
2018-04-06 10:41:09 -0300

    Testes separados por arquivo .h Agora cada modulo do sistema possui um arquivo de teste que pode ser executado sozinho ou simultaneamente com outro.

--- test/runner.cpp
+++ test/runner.cpp
@@ -20,51 +20,53 @@ int main( int argc, char *argv[] ) {
     return status;
 }
 bool suite_BodyHubTestSuite_init = false;
-#include "test_suite.h"
+#include "test_bodyhub.h"
 
 static BodyHubTestSuite suite_BodyHubTestSuite;
 
 static CxxTest::List Tests_BodyHubTestSuite = { 0, 0 };
-CxxTest::StaticSuiteDescription suiteDescription_BodyHubTestSuite( "test_suite.h", 8, "BodyHubTestSuite", suite_BodyHubTestSuite, Tests_BodyHubTestSuite );
+CxxTest::StaticSuiteDescription suiteDescription_BodyHubTestSuite( "test_bodyhub.h", 7, "BodyHubTestSuite", suite_BodyHubTestSuite, Tests_BodyHubTestSuite );
 
 static class TestDescription_suite_BodyHubTestSuite_test_class_variables : public CxxTest::RealTestDescription {
 public:
- TestDescription_suite_BodyHubTestSuite_test_class_variables() : CxxTest::RealTestDescription( Tests_BodyHubTestSuite, suiteDescription_BodyHubTestSuite, 24, "test_class_variables" ) {}
+ TestDescription_suite_BodyHubTestSuite_test_class_variables() : CxxTest::RealTestDescription( Tests_BodyHubTestSuite, suiteDescription_BodyHubTestSuite, 23, "test_class_variables" ) {}
  void runTest() { suite_BodyHubTestSuite.test_class_variables(); }
 } testDescription_suite_BodyHubTestSuite_test_class_variables;
 
 static class TestDescription_suite_BodyHubTestSuite_test_CHS : public CxxTest::RealTestDescription {
 public:
- TestDescription_suite_BodyHubTestSuite_test_CHS() : CxxTest::RealTestDescription( Tests_BodyHubTestSuite, suiteDescription_BodyHubTestSuite, 36, "test_CHS" ) {}
+ TestDescription_suite_BodyHubTestSuite_test_CHS() : CxxTest::RealTestDescription( Tests_BodyHubTestSuite, suiteDescription_BodyHubTestSuite, 35, "test_CHS" ) {}
  void runTest() { suite_BodyHubTestSuite.test_CHS(); }
 } testDescription_suite_BodyHubTestSuite_test_CHS;
 
 static class TestDescription_suite_BodyHubTestSuite_test_UHS : public CxxTest::RealTestDescription {
 public:
- TestDescription_suite_BodyHubTestSuite_test_UHS() : CxxTest::RealTestDescription( Tests_BodyHubTestSuite, suiteDescription_BodyHubTestSuite, 56, "test_UHS" ) {}
+ TestDescription_suite_BodyHubTestSuite_test_UHS() : CxxTest::RealTestDescription( Tests_BodyHubTestSuite, suiteDescription_BodyHubTestSuite, 55, "test_UHS" ) {}
  void runTest() { suite_BodyHubTestSuite.test_UHS(); }
 } testDescription_suite_BodyHubTestSuite_test_UHS;
+
+#include "test_sensorNode.h"
 
 static SensorNodeTestSuite suite_SensorNodeTestSuite;
 
 static CxxTest::List Tests_SensorNodeTestSuite = { 0, 0 };
-CxxTest::StaticSuiteDescription suiteDescription_SensorNodeTestSuite( "test_suite.h", 75, "SensorNodeTestSuite", suite_SensorNodeTestSuite, Tests_SensorNodeTestSuite );
+CxxTest::StaticSuiteDescription suiteDescription_SensorNodeTestSuite( "test_sensorNode.h", 5, "SensorNodeTestSuite", suite_SensorNodeTestSuite, Tests_SensorNodeTestSuite );
 
 static class TestDescription_suite_SensorNodeTestSuite_test_class_variables : public CxxTest::RealTestDescription {
 public:
- TestDescription_suite_SensorNodeTestSuite_test_class_variables() : CxxTest::RealTestDescription( Tests_SensorNodeTestSuite, suiteDescription_SensorNodeTestSuite, 95, "test_class_variables" ) {}
+ TestDescription_suite_SensorNodeTestSuite_test_class_variables() : CxxTest::RealTestDescription( Tests_SensorNodeTestSuite, suiteDescription_SensorNodeTestSuite, 25, "test_class_variables" ) {}
  void runTest() { suite_SensorNodeTestSuite.test_class_variables(); }
 } testDescription_suite_SensorNodeTestSuite_test_class_variables;
 
 static class TestDescription_suite_SensorNodeTestSuite_test_controllerFSM : public CxxTest::RealTestDescription {
 public:
- TestDescription_suite_SensorNodeTestSuite_test_controllerFSM() : CxxTest::RealTestDescription( Tests_SensorNodeTestSuite, suiteDescription_SensorNodeTestSuite, 106, "test_controllerFSM" ) {}
+ TestDescription_suite_SensorNodeTestSuite_test_controllerFSM() : CxxTest::RealTestDescription( Tests_SensorNodeTestSuite, suiteDescription_SensorNodeTestSuite, 36, "test_controllerFSM" ) {}
  void runTest() { suite_SensorNodeTestSuite.test_controllerFSM(); }
 } testDescription_suite_SensorNodeTestSuite_test_controllerFSM;
 
 static class TestDescription_suite_SensorNodeTestSuite_test_statusAnalysis : public CxxTest::RealTestDescription {
 public:
- TestDescription_suite_SensorNodeTestSuite_test_statusAnalysis() : CxxTest::RealTestDescription( Tests_SensorNodeTestSuite, suiteDescription_SensorNodeTestSuite, 124, "test_statusAnalysis" ) {}
+ TestDescription_suite_SensorNodeTestSuite_test_statusAnalysis() : CxxTest::RealTestDescription( Tests_SensorNodeTestSuite, suiteDescription_SensorNodeTestSuite, 54, "test_statusAnalysis" ) {}
  void runTest() { suite_SensorNodeTestSuite.test_statusAnalysis(); }
 } testDescription_suite_SensorNodeTestSuite_test_statusAnalysis;
 
---
+++ test/test_bodyhub.h
@@ -0,0 +1,72 @@
+#include <cxxtest/TestSuite.h>
+#include <stdint.h>
+#include "../bodyhub/include/BodyHubModule.h"
+            
+using namespace std;
+  
+class BodyHubTestSuite : public CxxTest::TestSuite{
+  public:
+    char* arg0;
+    char* arg1;
+    char* argv[3];
+    int32_t   argc;
+
+    void initialize_bh_instance(){
+      //inicializa a instância a ser tratada
+      arg0    = (char *) "main";
+      arg1    = (char *) "--cid=111";
+      argv[0] = &arg0[0];
+      argv[1] = &arg1[0];
+      argv[2] = NULL;
+      argc    = (int32_t)(sizeof(argv) / sizeof(argv[0])) - 1;
+    }
+    void test_class_variables( void ){     
+      cout << "\n\nTestes do BodyHub";
+      cout << "\n    Testando se variaveis de classe sao acessiveis: ";     
+
+      initialize_bh_instance();
+      BodyHubModule inst(argc, argv);
+      inst.m_id = 1;
+      inst.m_health_status = "baixo";
+      TS_ASSERT_EQUALS(inst.m_id, 1);
+      TS_ASSERT_EQUALS(inst.m_health_status, "baixo");    
+    }
+
+    void test_CHS (void) {
+      cout << "\n    Testando metodo 'calculateHealthStatus': ";   
+      initialize_bh_instance();
+      BodyHubModule inst(argc, argv);
+      inst.m_sensor[0] = "baixo";
+      inst.m_sensor[1] = "baixo";
+      inst.m_sensor[2] = "baixo";
+      TS_ASSERT_EQUALS(inst.calculateHealthStatus(), "bom");    
+
+      inst.m_sensor[0] = "moderado";
+      inst.m_sensor[1] = "moderado";
+      inst.m_sensor[2] = "moderado";
+      TS_ASSERT_EQUALS(inst.calculateHealthStatus(), "medio");    
+
+      inst.m_sensor[0] = "alto";
+      inst.m_sensor[1] = "alto";
+      inst.m_sensor[2] = "alto";
+      TS_ASSERT_EQUALS(inst.calculateHealthStatus(), "ruim");    
+    }
+
+    void test_UHS(void) {
+      timespec ts;
+      cout << "\n    Testando metodo 'updateHealthStatus': ";   
+
+      initialize_bh_instance();
+      BodyHubModule inst(argc, argv);    
+      inst.m_sensor[0] = "medio";
+      inst.m_sensor[1] = "baixo";
+      inst.m_sensor[2] = "baixo";
+
+      clock_gettime(CLOCK_REALTIME, &ts);
+      inst.updateHealthStatus(SensorData(0,1,"baixo",ts));
+      //Vê se recebe o mesmo status que foi enviado
+      TS_ASSERT_EQUALS(inst.m_sensor[0], "baixo");
+      //como os tres sinais sao baixos, o status deve ser bom
+      TS_ASSERT_EQUALS(inst.m_health_status, "bom");
+    }
+};
