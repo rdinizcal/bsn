d355d6c898558a164563a4060db42369a87427af
leooleo <leo.ms097@gmail.com>
2018-05-17 19:48:53 -0300

    Update script para criar os builds direito

--- script.sh
+++ script.sh
@@ -1,6 +1,7 @@
+mkdir -p libbsn/build
 mkdir -p simulation/communications/sockets/build 
 mkdir -p simulation/communications/build 
-mkdir -p sensor/build
+mkdir -p simulation/sensor/build
 mkdir -p module/bodyhub/build
 mkdir -p module/sensornode/build
 python3 script.py
