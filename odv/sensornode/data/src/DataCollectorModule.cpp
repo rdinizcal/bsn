849287a87b8c930dca45c946f34df2bfcd74c6e9
rdinizcal <rdinizcal@gmail.com>
2018-06-29 18:16:20 -0300

    Data generation methods extracted to new class DataCollector, with all files -.-

---
+++ odv/sensornode/data/src/DataCollectorModule.cpp
@@ -0,0 +1,35 @@
+#include "../include/DataCollectorModule.hpp"
+
+DataCollectorModule::DataCollectorModule(const int32_t &argc, char **argv) :
+    TimeTriggeredConferenceClientModule(argc, argv, "DataCollectorModule"),
+    mGeneratedData() {}
+
+DataCollectorModule::~DataCollectorModule() {}
+
+void DataCollectorModule::setUp() {}
+
+void DataCollectorModule::tearDown(){}
+
+odcore::data::dmcp::ModuleExitCodeMessage::ModuleExitCode DataCollectorModule::body(){
+
+    DataCollector dataCollector;
+
+    while (getModuleStateAndWaitForRemainingTimeInTimeslice() == odcore::data::dmcp::ModuleStateMessage::RUNNING) {
+        
+        //Gera o dado
+        mGeneratedData = dataCollector.generateDataByNormalDist(747.52, 102.4);
+
+        //Atribui dados à estrutura de dados especifica
+        RawData rawdata(mGeneratedData);
+        
+        //Encapsula dado
+        Container container(rawdata);
+
+        //Envia pacote
+        getConference().send(container);
+
+        std::cout << "Dado " << mGeneratedData << " gerado e enviado." << std::endl;
+    }
+
+    return odcore::data::dmcp::ModuleExitCodeMessage::OKAY;
+}
