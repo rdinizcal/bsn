bc3f7245c11e0ad3ce5ed4aa0bfccb234dcea43c
gabrielevi10 <gabrielevigomes@gmail.com>
2018-11-03 22:42:56 -0300

    arrumando exe.sh (mudei sem querer)

--- exe.sh
+++ exe.sh
@@ -14,10 +14,10 @@ fi
 # ${terminal_name} -e ./odv/centralhub/listener/build/tcp_listenerApp   --cid=222 --id=8083 &
 # ${terminal_name} -e ./odv/centralhub/processor/build/DataTriggeredApp --cid=222 &
 
-${terminal_name} -x ./odv/sensornode/collector/build/DataCollectorApp --cid=111 --id=0 &
-${terminal_name} -x ./odv/sensornode/collector/build/DataCollectorApp --cid=111 --id=1 &
-${terminal_name} -x ./odv/sensornode/filter/build/Filter --cid=111 &
-${terminal_name} -x ./odv/sensornode/sender/build/SenderApp --cid=111 --id=8080 
+${terminal_name} -e ./odv/sensornode/collector/build/DataCollectorApp --cid=111 --id=0 &
+${terminal_name} -e ./odv/sensornode/collector/build/DataCollectorApp --cid=111 --id=1 &
+${terminal_name} -e ./odv/sensornode/filter/build/Filter --cid=111 &
+${terminal_name} -e ./odv/sensornode/sender/build/SenderApp --cid=111 --id=8080 
 
 # ${terminal_name} -e ./odv/sensornode/collector/build/DataCollectorApp --cid=112  &
 # ${terminal_name} -e ./odv/sensornode/filter/build/Filter --cid=112 &
