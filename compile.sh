080d1ecbf40dde035edaafb6531941158aa0fd6e
rdinizcal <rdinizcal@gmail.com>
2018-12-18 23:36:12 -0200

    shell file to easily compile everything

---
+++ compile.sh
@@ -0,0 +1,27 @@
+bsn_folder=/home/rdiniz/projects/seams19/bsn
+
+mkdir -p libbsn/build
+gnome-terminal --working-directory=${bsn_folder}/libbsn/build -- bash -c 'cmake ..; sudo make install; bash'
+
+mkdir -p odv/sensornode/collector/build
+gnome-terminal --working-directory=${bsn_folder}/odv/sensornode/collector/build -- bash -c 'cmake ..; sudo make; bash'
+mkdir -p odv/sensornode/controller/build
+gnome-terminal --working-directory=${bsn_folder}/odv/sensornode/controller/build -- bash -c 'cmake ..; sudo make; bash'
+mkdir -p odv/sensornode/filter/build
+gnome-terminal --working-directory=${bsn_folder}/odv/sensornode/filter/build -- bash -c 'cmake ..; sudo make; bash'
+mkdir -p odv/sensornode/sender/build
+gnome-terminal --working-directory=${bsn_folder}/odv/sensornode/sender/build -- bash -c 'cmake ..; sudo make; bash'
+
+
+mkdir -p odv/centralhub/listener/build
+gnome-terminal --working-directory=${bsn_folder}/odv/centralhub/listener/build -- bash -c 'cmake ..; sudo make; bash'
+mkdir -p odv/centralhub/bsn/processor/build
+gnome-terminal --working-directory=${bsn_folder}/odv/centralhub/processor/build -- bash -c 'cmake ..; sudo make; bash'
+
+mkdir -p odv/monitor/resource/build
+gnome-terminal --working-directory=${bsn_folder}/odv/monitor/resource/build -- bash -c 'cmake ..; sudo make; bash'
+
+mkdir -p odv/persister/txt/build
+gnome-terminal --working-directory=${bsn_folder}/odv/persister/txt/build -- bash -c 'cmake ..; sudo make; bash'
+
+
