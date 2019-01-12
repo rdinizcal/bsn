278fd1b50127babe20662bce9501d08548581786
rdinizcal <rdinizcal@gmail.com>
2019-01-12 14:40:12 -0200

    plot figure for send round results

--- launch.sh
+++ launch.sh
@@ -3,7 +3,7 @@ bsn_folder=$PWD
 gnome-terminal --working-directory=${bsn_folder}/odv/sim/ -e 'sudo odsupercomponent --cid=111 --managed=simulation_rt --freq=1'      & sleep 3s
 
 # execute manager node (must be executed first)
-gnome-terminal --working-directory=${bsn_folder}/odv/sim/controller/build -e "./manager --cid=111"                          & sleep 1s
+gnome-terminal --working-directory=${bsn_folder}/odv/sim/controller/build -e "./manager --cid=111"                           & sleep 1s
 
 # execute sensor node
 gnome-terminal --working-directory=${bsn_folder} -e "./odv/sim/sensors/ecg/build/ecg --cid=111"                              & sleep 1s
