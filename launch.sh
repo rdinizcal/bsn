86893036690f06922386cfc9a063a95867d4e459
rdinizcal <rdinizcal@gmail.com>
2019-01-15 15:37:17 -0200

    manager -> controller refactoring

--- launch.sh
+++ launch.sh
@@ -2,8 +2,8 @@ bsn_folder=$PWD
 
 gnome-terminal --working-directory=${bsn_folder}/odv/sim/ -e 'sudo odsupercomponent --cid=111 --managed=simulation_rt --freq=1'      & sleep 3s
 
-# execute manager node (must be executed first)
-gnome-terminal --working-directory=${bsn_folder}/odv/sim/controller/build -e "./manager --cid=111"                           & sleep 1s
+# execute controller node (must be executed first)
+gnome-terminal --working-directory=${bsn_folder}/odv/sim/controller/build -e "./controller --cid=111"                           & sleep 1s
 
 # execute sensor node
 gnome-terminal --working-directory=${bsn_folder} -e "./odv/sim/sensors/ecg/build/ecg --cid=111"                              & sleep 1s
