4ee0edaebd24e3446fbdb17ecc6b89af584eea13
rdinizcal <rdinizcal@gmail.com>
2019-01-27 18:46:02 -0200

    new legend, rmv setpoint annotation

--- experiment/4a/r_proj/plot4B.R
+++ experiment/4a/r_proj/plot4B.R
@@ -350,14 +350,14 @@ scaleFUN <- function(x) sprintf("%.2f", x)
 nt_as_c_mc_reli <- ggplot() +
   #geom_line(data=nt_as_c_m_data, aes(TIME..ms./1000,(OXIM+ECG+TEMP+ABP)*25, color = "available sensors"), size=0.5) +
   geom_hline(yintercept = rmin*100, color = 'black', linetype=2, alpha=0.3) +
-  geom_hline(yintercept = r*100, color = 'black') +
+  geom_hline(aes(yintercept = r*100, color = "set point")) +
   geom_hline(yintercept = rmax*100, color = 'black', linetype=2, alpha=0.3) +
   #geom_line(data=nt_as_c_m_data, aes(TIME..ms./1000, RELIABILITY*100, color="untamed"), size=1) +
   stat_smooth(data=nt_as_c_m_data, aes(TIME..ms./1000, RELIABILITY*100, color="untamed"), method = lm, formula = y ~ poly(x, 25), se = FALSE) +
   #geom_line(data=t_a_m_data, aes(TIME..ms./1000, RELIABILITY*100, color="tamed"), size=1) +
   stat_smooth(data=t_a_m_data, aes(TIME..ms./1000, RELIABILITY*100, color="tamed"), method = lm, formula = y ~ poly(x, 25), se = FALSE) +
-  geom_text(aes(5, rmin*100, label = 'setpoint', vjust = -1), size=3)+
-  scale_color_manual(values=c("tamed"='blue',"untamed"='red'), name = "") +
+  #geom_text(aes(5, rmin*100, label = 'setpoint', vjust = -1), size=3)+
+  scale_color_manual(values=c("tamed"='blue',"untamed"='red', "set point"='black'), name = "") +
   scale_y_continuous(breaks=sort(c(seq(0,100,length.out=5), r*100))) +
   labs(x="", y="reliability (%)") +  
   theme(legend.position='bottom', axis.text=element_text(size=10)) +
@@ -372,8 +372,8 @@ nt_as_c_mc_cost <- ggplot() +
   stat_smooth(data=nt_as_c_m_data, aes(TIME..ms./1000, COST, color="untamed"), method = lm, formula = y ~ poly(x, 25), se = FALSE) +
   #geom_line(data=t_a_m_data, aes(TIME..ms./1000, COST, color="tamed"), size=1) +
   stat_smooth(data=t_a_m_data, aes(TIME..ms./1000, COST, color="tamed"), method = lm, formula = y ~ poly(x, 25), se = FALSE) +
-  geom_text(aes(5, cmin, label = 'setpoint', vjust = -1), size=3)+
-  scale_color_manual(values=c("tamed"='blue',"untamed"='red'), name = "") +
+  #geom_text(aes(5, cmin, label = 'setpoint', vjust = -1), size=3)+
+  scale_color_manual(values=c("tamed"='blue',"untamed"='red', "set point"='black'), name = "") +
   scale_y_continuous(labels=scaleFUN, breaks=sort(c(seq(0,0.7,length.out=5), c))) +
   labs(x="time (s)", y="cost (W)") +  
   theme(legend.position='none', axis.text=element_text(size=10)) +
