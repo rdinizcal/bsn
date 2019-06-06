bcc9422d72ac87560419649f0944db0a78aa5cb2
Jorge <jorge.jglm@gmail.com>
2019-06-06 19:02:05 -0300

    vários testes

--- libbsn/include/sensorfault/RandomSensorFault.hpp
+++ libbsn/include/sensorfault/RandomSensorFault.hpp
@@ -13,8 +13,8 @@ namespace bsn {
             std::default_random_engine gerador;
             std::normal_distribution<double> distribuicaoErro;
             const unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
-            //chanceFalha = 0.10, chancesucesso = 0.90 para dist > z;    
-            const double z = 1.29;
+            //chanceFalha = 0.05, chancesucesso = 0.95 para dist > z;    
+            const double z = 1.65;
         };
     }
 }
